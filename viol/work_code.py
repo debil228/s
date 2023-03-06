dbb34808a016442496ea90c01f0ab82339459119
debil228 <31446926+debil228@users.noreply.github.com>
2023-03-07 00:05:58 +0600

    Update work_code.py

--- viol/work_code.py
+++ viol/work_code.py
@@ -12,7 +12,7 @@ from key import gpt_work
 
 import openai
 
-open_tok = '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'
+open_tok = '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'
 
 openai.api_key = open_tok
 
